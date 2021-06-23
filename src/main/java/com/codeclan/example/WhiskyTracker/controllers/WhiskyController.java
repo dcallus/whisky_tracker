37a50eaf9758928938a1a26e523c62cbf4ccc4cb
Daniel Callus <danielcallus@gmail.com>
2021-06-23 16:19:20 +0100

    stuff

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,44 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(@RequestParam(name = "distillery", required=false) String distillery,
+                                                       @RequestParam(name = "year", required=false) Integer year) {
+        if (year != null && distillery == null) {
+            return new ResponseEntity<>(whiskyRepository.findByYearIs(year), HttpStatus.OK);
+        }else{
+            if (year != null && distillery != null){
+                return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(distillery, age))
+            }
+        }
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/{id}")
+    public ResponseEntity getWhiskies(@PathVariable Long id){
+        return new ResponseEntity<>(whiskyRepository.findById(id), HttpStatus.OK);
+    }
+
+//    @GetMapping(value = "/whiskies/{year}")
+//    public ResponseEntity getWhiskies(@RequestParam(name = ), @PathVariable int year){
+//        public ResponseEntity<List<Whisky>> getAllWhiskies(@RequestParam(name = "distillery", required=false) String name)
+//        return new ResponseEntity<>(whiskyRepository.findByYearIs(year), HttpStatus.OK);
+//    }
+
+
+    //"whiskies?distillery=glengoyne&1995"
+}
